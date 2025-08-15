0d6e3f553502513cca2c9f1bc9f840be4b801110
kei-satsu <yephoneaungmdl@gmail.com>
2025-08-15 18:51:20 +0630

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+<!-- Add this before </body> -->
+<script>
+  const searchBtn = document.querySelector('.search-box button');
+  const searchInput = document.querySelector('.search-box input');
+
+  searchBtn.addEventListener('click', () => {
+    alert(`Searching for: ${searchInput.value}`);
+  });
+</script>
 
